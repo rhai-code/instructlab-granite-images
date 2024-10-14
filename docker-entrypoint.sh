b3a0449de2e7cfb3585e0da5b5cd0e9f516d88ad
Philip <philip@deewhy.ie>
2024-10-14 14:50:24 -0400

    Added CLI mode

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 set -eu
 
+server_mode=false
+
+usage() {
+ echo "Usage: $0 [OPTIONS]"
+ echo "Options:"
+ echo " -h, --help      Display this help message"
+ echo " -s, --serve     Enable server mode"
+}
 
 set_default_env_vars() {
-  if [ -z ${LLAMA_HOST+x} ]; then
-    export LLAMA_HOST="0.0.0.0"
-  fi
+
   if [ -z ${LLAMA_N_GPU_LAYERS+x} ]; then
     export LLAMA_N_GPU_LAYERS=99
   fi
@@ -33,5 +39,34 @@ convert_llama_env_vars() {
 set_default_env_vars
 convert_llama_env_vars
 
-set -x
-llama-server  -m $MODEL_PATH  $LLAMA_ARGS
+handle_options() {
+  while [ $# -gt 0 ]; do
+    case $1 in
+      -h | --help)
+      echo 'hi help'
+        usage
+        exit 0
+        ;;
+      -s | --serve)
+        set -x
+        server_mode=true
+        ;;
+      *)
+        echo "Invalid option: $1" >&2
+        usage
+        exit 1
+        ;;
+    esac
+    shift
+  done
+}
+
+handle_options "$@"
+
+if [ "$server_mode" = true ]; then
+llama-server -m $MODEL_PATH  $LLAMA_ARGS --host 0.0.0.0
+else
+  llama-cli -m $MODEL_PATH -p "You are a helpful assistant" -cnv $LLAMA_ARGS
+fi
+
+
